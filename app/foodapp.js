42fb1c261a0c222cc2aea006e9fc5a71662d098c
euler456 <47268086+euler456@users.noreply.github.com>
2021-04-02 03:55:00 +1100

    ddd

--- app/foodapp.js
+++ app/foodapp.js
@@ -12,3 +12,35 @@ fetch('http://localhost/apitesting/api/fooditemfunction.php').then((res)=>res.js
     }
     document.querySelector('.tbody').innerHTML = output;
 }).catch(error=>console.error(error));
+
+
+document.getElementById('Addfood').addEventListener('submit', function(e) {fetchAddfood(e)});
+function fetchAddfood(evt) {
+    evt.preventDefault();
+    var fd = new FormData();
+    fd.append('foodname', foodname.value);
+    fd.append('description', description.value); //lop off # in hex code
+    fd.append('images_path', images_path.value);
+    fd.append('options', options.value);
+    fd.append('Price', Price.value);
+    fd.append('csrf', localStorage.getItem('csrf'));
+    fetch('http://localhost/apitesting/api/userapi.php?action=register', 
+    {
+        method: 'POST',
+        body: fd,
+        credentials: 'include'
+    })
+    .then(function(headers) {
+        if(headers.status == 400) {
+            console.log('user exists');
+            return;
+        }
+     
+        if(headers.status == 201) {
+            console.log('registration updated');
+            return;
+        }
+       
+    })
+    .catch(function(error) {console.log(error)});
+}
