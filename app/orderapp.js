4adef470c7a149362dea02fb67c02b0cf89443a2
euler456 <47268086+euler456@users.noreply.github.com>
2021-04-12 14:35:07 +1000

    gg

--- app/orderapp.js
+++ app/orderapp.js
@@ -10,12 +10,13 @@ fetch('http://localhost/apitesting/api/orderapi.php?action=displayorderfood',
     let output = '';
     for(let i in response){
         output+=`<tr>
-        <td>${response[i].F_ID}</td>
+        <td class='fd-name'>${response[i].F_ID}</td>
         <td>${response[i].foodname}</td>
         <td ><img src='../images/${response[i].image }' style="width: 100px; height: 100px;"></td>
         <td>${response[i].price}</td>
-        <td><input type="number" name="quantity" value="1"  onclick="fetchorder(${response[i].F_ID},this)"></td>
+        <td><input type="number" class="fd-value" name="quantity" value="0" min="0" max="100"></td>
         <td>${response[i].options}</td>
+        <td><button class="btnSelect">Select</button></td>
         </tr>`;
     }
     document.querySelector('.tbody').innerHTML = output;
@@ -26,3 +27,14 @@ function fetchorder(F_ID,foodvalue){
 
     
 }
+$(document).ready(function(){
+    
+    $("#orderform").on('click', '.btnSelect', function() {
+      var currentRow = $(this).closest("tr");
+      var col1 = currentRow.find(".fd-value").val(); 
+      var col2 = currentRow.find(".fd-name").html(); 
+  
+
+      
+    });
+  });
